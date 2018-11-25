e7d9828550360cd3e2f3bb2b4c03ff63ab2ad68d
wilson-moreno <wilson.moreno.ph@gmail.com>
2018-11-25 20:54:42 +0800

    Pictures added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
        def full_title(page_title = '')
-           base_title = "LeybiArt"
+           base_title = "Creatively"
 
            if page_title.empty?
               base_title
